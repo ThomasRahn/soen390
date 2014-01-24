74367f50657094128c1431d3c38fa0a38f578ac6
Dror <dror.ozgaon@gmail.com>
2014-01-24 02:47:47 +0000

    Added login view and associated styles.

--- soen390/app/views/login.blade.php
+++ soen390/app/views/login.blade.php
@@ -1,19 +1,38 @@
 <html>
 <head>
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/home/o_dror/soen390/soen390/app/css/stylesheet.css" type="text/css" rel="stylesheet">
+	<link href="/css/style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-	<div class="container">
-		<form class="form-signin" id="loginForm" onsubmit="return validateLoginForm()" method="post">
-			<h2 class="form-signin-heading">Login</h2>
-			<input type="text" class="input-block-level" placeholder="Email Address" name="email">	
-			<input type="password" class="input-block-level" placeholder="Password" name="password">
-			<button class="btn btn-large btn-primary" type="submit">Login</button>
-			<br /><br />
-			<div id="errorMessage">
-			</div>
-		</form>
+<div class="container">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+	    	<h3 class="panel-title">Login</h3>
+	  	</div>
+	  	<div class="panel-body">
+				<form class="form-horizontal" role="form">
+			  		<div class="form-group">
+			    		<label for="email" class="col-sm-2 control-label">Email</label>
+			    		<div class="col-sm-10">
+			      			<input type="email" class="form-control input-lg" id="email" placeholder="Email">
+			    		</div>
+			  		</div>
+
+			  		<div class="form-group">
+			    		<label for="password" class="col-sm-2 control-label">Password</label>
+			    		<div class="col-sm-10">
+			      			<input type="password" class="form-control input-lg" id="password" placeholder="Password">
+			    		</div>
+			  		</div>
+
+			  		<div class="form-group">
+			    		<div class="col-sm-offset-2 col-sm-10">
+			      			<button type="submit" class="btn btn-primary btn-lg">Sign In</button>
+			    		</div>
+			  		</div>
+				</form>
+	  	</div>
 	</div>
+</div>
 </body>
 </html>
