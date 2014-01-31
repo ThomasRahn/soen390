626d8b07e304fb7643de320ebda7cca420530854
Alan Ly <me@alanly.ca>
2014-01-31 11:38:07 -0500

    Reworked Shahrad's interface into a new design with player popup.

---
+++ soen390/public/js/dictionary.js
@@ -0,0 +1,16 @@
+var dictionary = {
+                    "en": {
+                        "brand": 'You <i class="fa fa-comments"></i> Deliberate',
+                        "brandText": 'You Deliberate',
+                        "stance": 'Stance',
+                        "popularity": 'Popularity',
+                        "spaghetti": "Arm Spaghetti"
+                    },
+                    "fr": {
+                        "brand": 'Vous <i class="fa fa-comments"></i> D&eacute;lib&eacute;r&eacute;z',
+                        "brandText": 'Vous Délibéréz',
+                        "stance": 'Position',
+                        "popularity": 'Popularit&eacute;',
+                        "spaghetti": "Spaghetti de bras"
+                    }
+                }
