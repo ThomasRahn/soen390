eb5d266283cca4eea862710280dcec14ce072277
Alan Ly <me@alanly.ca>
2014-02-08 14:19:46 -0500

    Added a convenience method for alerts.

--- soen390/app/controllers/BaseController.php
+++ soen390/app/controllers/BaseController.php
@@ -15,4 +15,22 @@ class BaseController extends Controller {
 		}
 	}
 
+	/**
+	 * Flashses the result of a request action for later display in the view.
+	 * Data is available from the Session object in `action.failed` and `action.message` keys.
+	 * An optional `RedirectorResponse` can be provided if a single line alert-redirect action is preferred.
+	 *
+	 * @param  $hasFailed  boolean
+	 * @param  $message    string
+	 * @param  $redirector RedirectResponse
+	 * @return RedirectResponse
+	 */
+	protected function alertAction($hasFailed, $message, RedirectResponse $redirector = null)
+	{
+		Session::flash('action.failed', $hasFailed);
+		Session::flash('action.message', $message);
+
+		return $redirector;
+	}
+
 }
