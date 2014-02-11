e6cd1072ba7e578c508a7eaf34dae014f94f327b
Thomas <thomas@rahn.ca>
2014-02-11 04:40:48 +0000

    removed belongs to

--- soen390/app/models/Comment.php
+++ soen390/app/models/Comment.php
@@ -11,7 +11,7 @@ class Comment extends Eloquent{
 	public function narrative(){
 		return $this->belongsTo("Narrative", "NarrativeID","NarrativeID");
 	}
-	public function comment(){
-		return $this->belongsTo("Comment", "CommentParentID", "CommentID");
-	}
+//	public function comment(){
+//		return $this->belongsTo("Comment", "CommentParentID", "CommentID");
+//	}
 }
