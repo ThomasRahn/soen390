51b9afad6af9d146928c318081c31904247ea7d8
Alan Ly <me@alanly.ca>
2014-02-16 21:53:33 +0000

    Refactored to coding standards.

--- soen390/app/models/Narrative.php
+++ soen390/app/models/Narrative.php
@@ -1,21 +1,25 @@
 <?php 
 
-class Narrative extends Eloquent{
-	
-	protected $table = "Narrative";
-	public static $unguarded = true;
-	public $timestamps = false;
+class Narrative extends Eloquent
+{
+	protected $table = 'Narrative';
 	protected $primaryKey = 'NarrativeID';
 	protected $softDelete = true;
+	protected $guarded = array('id');
+	public $timestamps = false;
 
-	public function category(){
-		return $this->belongsTo("Category", "CategoryID", "CategoryID");
-	}
-	public function langauge(){
-		return $this->belongsTo("Language", "LanguageID", "LanguageID");
-	}
-	public function content(){
-		return $this->hasMany("Content", "NarrativeID","NarrativeID");
+	public function category()
+	{
+		return $this->belongsTo('Category', 'CategoryID', 'CategoryID');
 	}
 
+	public function langauge()
+	{
+		return $this->belongsTo('Language', 'LanguageID', 'LanguageID');
+	}
+
+	public function content()
+	{
+		return $this->hasMany('Content', 'NarrativeID', 'NarrativeID');
+	}
 }
