2cb7352746427c63b6e907d163844e892871183c
Alan Ly <me@alanly.ca>
2014-02-17 23:34:49 +0000

    Correct mistake with Sonus facade alias.

--- soen390/app/config/app.php
+++ soen390/app/config/app.php
@@ -173,7 +173,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Sonus'			  => 'Rafasamp\Sonus\SonusServiceProvider',
+		'Sonus'		  => 'Rafasamp\Sonus\Facade',
 
 	),
 
