165bde0fd65dcfdb696c84e0c13a4fe21ff5e2ca
Alan Ly <me@alanly.ca>
2014-02-20 19:13:37 +0000

    Calculate duration of each audio file.

--- soen390/app/models/TranscodeAudio.php
+++ soen390/app/models/TranscodeAudio.php
@@ -22,6 +22,12 @@ class TranscodeAudio
 			'mp3' => 'libmp3lame',
 		);
 
+		// Determine the duration of the source audio.
+		$durationString = Sonus::getMediaInfo($sourceFilePath)['format']['duration'];
+		sscanf($durationString, "%d:%d:%d.%s", $dHours, $dMinutes, $dSeconds, $dMicroseconds);
+		$dSeconds += ($dHours * 3600) + ($dMinutes * 60);
+		$parsedDuration = $dSeconds . '.' . $dMicroseconds;
+
 		// Create a version of the source file for each format specified.
 		foreach ($outputFormats as $extension => $codec) {
 			// Determine the output basename
@@ -44,6 +50,7 @@ class TranscodeAudio
 			Content::create(array(
 				'NarrativeID' => $narrativeID,
 				'AudioPath' => $baseName,
+				'Duration' => $parsedDuration,
 			));
 		}
 
