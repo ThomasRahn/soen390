19080a22f7b7e3cceffb6383ecad750393a90ab9
Alan Ly <me@alanly.ca>
2014-02-21 00:45:07 +0000

    Created Media model, replacement for Content.

---
+++ soen390/app/database/migrations/2014_02_20_232505_create_media_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->integer('narrative_id')->nullable();
+			$table->integer('topic_id')->nullable();
+			$table->integer('comment_id')->nullable();
+			$table->string('type');
+			$table->string('filename');
+			$table->string('basename');
+			$table->string('audio_duration')->nullable();
+			$table->string('audio_codec')->nullable();
+
+			$table->index('narrative_id');
+			$table->index('filename');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('media');
+	}
+
+}
