23be1051f1edaec3a306ceb9f708e079b8e19674
Thomas <thomas@rahn.ca>
2014-02-27 02:29:57 +0000

    finished Narrative relationship tests

--- soen390/app/tests/NarrativeTest.php
+++ soen390/app/tests/NarrativeTest.php
@@ -71,5 +71,99 @@ class NarrativeTest extends TestCase
         $this->assertNull($narrativeFetched);
 
     }
+    /*
+    *   Testing the relationship between narative and category
+    *
+    *  @covers Narrative::category
+    */
+    public function testNarrativeCategory(){
+         $narrativeCreated = new Narrative;
 
+        $date = date('Y-m-d H:i:s');
+
+        $narrativeCreated->TopicID = 1;
+        $narrativeCreated->CategoryID = 1;
+        $narrativeCreated->LanguageID = 1;
+        $narrativeCreated->DateCreated = $date;
+        $narrativeCreated->Name = "Test";
+        $narrativeCreated->Agrees = 1;
+        $narrativeCreated->Disagrees = 1;
+        $narrativeCreated->Indifferents = 1;
+        $narrativeCreated->Published = true;
+
+        $narrativeCreated->save();
+
+        $category = new Category;
+        $category->CategoryID = 1;
+        $category->Description = "hello";
+        $category->save();
+
+        $category1 = Narrative::find(1)->category()->first();
+        $this->assertNotNull($category1);
+
+    }
+      /*
+    *   Testing the relationship between narative and Language
+    *
+    *  @covers Narrative::language
+    */
+    public function testNarrativeLanguage(){
+         $narrativeCreated = new Narrative;
+
+        $date = date('Y-m-d H:i:s');
+
+        $narrativeCreated->TopicID = 1;
+        $narrativeCreated->CategoryID = 1;
+        $narrativeCreated->LanguageID = 1;
+        $narrativeCreated->DateCreated = $date;
+        $narrativeCreated->Name = "Test";
+        $narrativeCreated->Agrees = 1;
+        $narrativeCreated->Disagrees = 1;
+        $narrativeCreated->Indifferents = 1;
+        $narrativeCreated->Published = true;
+
+        $narrativeCreated->save();
+
+        $language = new Language;
+        $language->LanguageID = 1;
+        $language->Description = "hello";
+        $language->save();
+
+        $langauge1 = Narrative::find(1)->language()->first();
+        $this->assertNotNull($langauge1);
+
+    }
+  /*
+    *   Testing the relationship between narative and Media Item
+    *
+    *  @covers Narrative::media
+    */
+    public function testNarrativeMedia(){
+         $narrativeCreated = new Narrative;
+
+        $date = date('Y-m-d H:i:s');
+
+        $narrativeCreated->TopicID = 1;
+        $narrativeCreated->CategoryID = 1;
+        $narrativeCreated->LanguageID = 1;
+        $narrativeCreated->DateCreated = $date;
+        $narrativeCreated->Name = "Test";
+        $narrativeCreated->Agrees = 1;
+        $narrativeCreated->Disagrees = 1;
+        $narrativeCreated->Indifferents = 1;
+        $narrativeCreated->Published = true;
+
+        $narrativeCreated->save();
+
+        $media = new Media;
+        $media->id = 1;
+        $media->filename = "hello";
+        $media->basename = "hello";
+        $media->type = "hello";
+        $media->save();
+
+        $media1 = Narrative::find(1)->media();
+        $this->assertNotNull($media1);
+
+    }
 }
