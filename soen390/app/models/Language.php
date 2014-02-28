aa846e639be2497cbf7ae3bc7c98eb341eb0dbe8
Alan Ly <me@alanly.ca>
2014-02-28 15:47:28 +0000

    Refactored indentation for readability.

--- soen390/app/models/Language.php
+++ soen390/app/models/Language.php
@@ -6,11 +6,14 @@
 class Language extends Eloquent
 {
 
+	protected $table      = 'Language';
 	protected $primaryKey = 'LanguageID';
+	public    $timestamps = false;
+    public    $guarded    = array('LanguageID');
 
-	public $timestamps = false;
-
-	public function narrative(){
+	public function narrative()
+    {
 		return $this->hasMany('Narrative', 'LanguageID', 'LanguageID');
 	}
+
 }
