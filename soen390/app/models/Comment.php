33066d471eb3b1055a22ed52e9ae08923af985b9
Alan Ly <me@alanly.ca>
2014-02-28 15:43:44 +0000

    Removed unused functionality.

--- soen390/app/models/Comment.php
+++ soen390/app/models/Comment.php
@@ -16,7 +16,5 @@ class Comment extends Eloquent
     {
 		return $this->belongsTo("Narrative", "NarrativeID","NarrativeID");
 	}
-//	public function comment(){
-//		return $this->belongsTo("Comment", "CommentParentID", "CommentID");
-//	}
+
 }
