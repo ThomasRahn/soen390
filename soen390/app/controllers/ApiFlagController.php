08cf22396b98223f6fdb009d55fe03ec71ea7817
Alan Ly <me@alanly.ca>
2014-03-01 21:01:52 +0000

    Reformatted and cleaned up code for ApiFlagController.

--- soen390/app/controllers/ApiFlagController.php
+++ soen390/app/controllers/ApiFlagController.php
@@ -2,65 +2,39 @@
 
 class ApiFlagController extends \BaseController {
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	public function __construct()
-	{
-		$this->beforeFilter('auth', array(
-			'except' => array('index', 'show'))
-		);
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    public function __construct()
+    {
+        $this->beforeFilter('auth', array(
+            'except' => array('index', 'show'))
+        );
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$NarrativeID = Input::get('NarrativeID');
-		$flags = Flag::where('NarrativeID',$NarrativeID)->get();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $narrative = Input::get('NarrativeID');
 
-		$formattedFlags = array();
-		foreach ($flags as $flag) {
-			$formattedFlags[] = array(
-					'id' => $flag->FlagID,
-					'name' => $flag->narrative()->first()->Name,
-					'narrativeID' => $flag->NarrativeID,
-					'comment' =>$flag->Comment,
-				);
-		}
-		return Response::json($formattedFlags);
-	}
-	
-	/*
-	*  Returns the number of flags per narrative
-	*
-	*
-	*/
-	public function getCount($NarrativeID){
-		
-	}
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+        $flags = Flag::where('NarrativeID', $narrative)->with('narrative')->get();
 
-	/**
-	 * Store the specified narrative.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
+        $flagArray = array();
 
-	}
+        foreach ($flags as $flag) {
+            $flagArray[] = array(
+                    'id'          => $flag->FlagID,
+                    'name'        => $flag->narrative()->first()->Name,
+                    'narrativeID' => $flag->NarrativeID,
+                    'comment'     => $flag->Comment,
+                );
+        }
+
+        return Response::json($flagArray);
+    }
 
 }
