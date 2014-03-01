372999a2961e40dedf36f71a25aed61321c17247
Alan Ly <me@alanly.ca>
2014-03-01 20:32:41 +0000

    Added guard for TopicID assignment.

--- soen390/app/models/Topic.php
+++ soen390/app/models/Topic.php
@@ -8,6 +8,7 @@ class Topic extends Eloquent
 
     protected $table      = 'Topic';
     protected $primaryKey = 'TopicID';
+    protected $guarded    = array('TopicID');
     public    $timestamps = false;
 
 }
