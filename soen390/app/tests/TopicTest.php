f7b3ae6eabf81c795c661f08be44cd86353cc6a1
Alan Ly <me@alanly.ca>
2014-03-02 01:00:55 +0000

    Removed commented test units and added covers annotations.

--- soen390/app/tests/TopicTest.php
+++ soen390/app/tests/TopicTest.php
@@ -2,44 +2,19 @@
 
 class TopicTest extends TestCase
 {
-    
-    /**
-     * Test the API's index and ensures that response is valid JSON.
-     *
-     */
-    public function testIndex()
-    {
-        /*$response = $this->call('GET', 'api/topic');
 
-        $this->assertResponseOk();
-
-        json_decode($response->getContent());
-
-        $this->assertEquals(JSON_ERROR_NONE, json_last_error());*/
-    }
-    /**
-     * Ensure Topics get fetched.
-     *
-     */
-    public function testTopicRetrieval()
-    {
-        /*$narratives = Narrative::all();
-
-        $this->assertNotEmpty($narratives);*/
-
-    }
     /**
      * Ensure Topic gets created.
-     * @covers Topic::create
+     *
      * @covers Topic::save
      * @covers Topic::delete
-     *
+     * @covers Topic::find
      */
     public function testCategoryCreation()
     {
         $topicCreated = new Topic;
 
-        $date = date_create_from_format('j-M-Y', '1-Jan-2000');
+        $date = new DateTime;
 
         $topicCreated->Description = "Test";
         $topicCreated->DateCreated = $date;
