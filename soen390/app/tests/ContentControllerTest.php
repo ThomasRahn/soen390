ba2e5d82e50ae3c5d6ddcb9e004d57be13dea0f9
Alan Ly <me@alanly.ca>
2014-03-02 00:46:54 +0000

    Partial coverage for ContentController and NarrativeController.

--- soen390/app/tests/ContentControllerTest.php
+++ soen390/app/tests/ContentControllerTest.php
@@ -3,20 +3,27 @@
 class ContentControllerTest extends TestCase
 {
 
-	/*
-	*
-	*
-	* ContentController::getContent
-	*/
-	public function testGetContent(){
-		$media = new Media;
-        $media->id = 1;
-        $media->filename = "1.mp3";
-        $media->basename = "test";
-        $media->type = "audio";
-        $media->save();
+    /**
+     * Attempt to get a media file that doesn't exist.
+     *
+     * @covers            ContentController::getContent
+     * @expectedException Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function testGetNonExistantContent()
+    {
+        $response = $this->action('GET', 'ContentController@getContent', array('id' => 10));
 
+        $this->assertResponseStatus(404);
+    }
 
-	}
+    /**
+     * Attempt to get an existant media file.
+     *
+     * @covers ContentController::getContent
+     */
+    public function testGetExistingContent()
+    {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
 
 }
