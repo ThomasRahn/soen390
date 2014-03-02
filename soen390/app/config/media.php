043124e273f8155e1292b94a59994de03f841b8d
Alan Ly <me@alanly.ca>
2014-03-02 16:20:35 +0000

    Added transcode option to configuration.

--- soen390/app/config/media.php
+++ soen390/app/config/media.php
@@ -2,6 +2,12 @@
 
 return array(
 
+    /**
+     * Defines whether or not uploaded audio should be transcoded.
+     * If set to false, the original source audio will just be moved.
+     */
+    'transcode' => true,
+
 	'paths' => array(
 		'uploads' => __DIR__ . '/../storage/media/uploads',
 		'extracted' => __DIR__ . '/../storage/media/extracted',
