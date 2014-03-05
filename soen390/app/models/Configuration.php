146aa16716afe9337502d63f713d12eb87f00e86
Alan Ly <me@alanly.ca>
2014-03-05 19:57:33 +0000

    Added migration and model for configuration store.

---
+++ soen390/app/models/Configuration.php
@@ -0,0 +1,46 @@
+<?php
+
+class Configuration extends \Eloquent
+{
+
+    protected $guarded    = array();
+    protected $primaryKey = 'key';
+    protected $table      = 'configuration';
+
+    /**
+     * Retrieves the value for a particular configuration setting.
+     *
+     * @param  string  $key
+     * @return string
+     */
+    public static function get($key, $default = '')
+    {
+        $config = parent::find($key);
+
+        return (! $config) ? $default : $config->value;
+    }
+
+    /**
+     * Stores the $value for configuration $key.
+     *
+     * @param  string  $key
+     * @param  string  $value
+     * @return boolean
+     */
+    public static function set($key, $value)
+    {
+        $config = parent::find($key);
+
+        if (! $config) {
+            return parent::create(array(
+                'key'   => $key,
+                'value' => $value,
+            ));
+        } else {
+            $config->value = $value;
+
+            return $config->save();
+        }
+    }
+
+}
