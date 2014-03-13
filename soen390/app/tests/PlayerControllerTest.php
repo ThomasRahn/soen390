f605a4ad541277cc5b53d16ece310fcd55f9ffc7
Alan Ly <me@alanly.ca>
2014-03-13 01:38:33 +0000

    Removed separate player comment view.

--- soen390/app/tests/PlayerControllerTest.php
+++ soen390/app/tests/PlayerControllerTest.php
@@ -18,20 +18,6 @@ class PlayerControllerTest extends TestCase
     }
 
     /**
-     * Attempt to retrieve the comments view for an invalid narrative ID.
-     * Should result in a 404 response and exception.
-     *
-     * @covers PlayerController::getComments
-     * @expectedException \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-     */
-    public function testGetCommentsWithInvalidId()
-    {
-        $response = $this->action('GET', 'PlayerController@getComments', array('id' => 70));
-
-        $this->assertResponseStatus(404);
-    }
-
-    /**
      * Attempt to retrieve the player with a valid narrative ID.
      *
      * @covers PlayerController::getPlay
@@ -48,24 +34,6 @@ class PlayerControllerTest extends TestCase
         $this->assertEquals('player.popup', $response->original->getName());
         $this->assertViewHas('apiPath');
         $this->assertViewHas('commentFramePath');
-    }
-
-    /**
-     * Attempt to retrieve the comments view with a valid narrative ID.
-     *
-     * @covers PlayerController::getComments
-     */
-    public function testGetCommentsWithValidId()
-    {
-        $this->addNarrativeToDatabase();
-
-        $id = Narrative::first()->NarrativeID;
-
-        $response = $this->action('GET', 'PlayerController@getComments', array('id' => $id));
-
-        $this->assertResponseOk();
-        $this->assertEquals('player.comments', $response->original->getName());
-        $this->assertViewHas('apiPath');
     }
 
 }
