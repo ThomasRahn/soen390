cb4693041150126c13bdac0888b279b6bbfb83a6
Alan Ly <me@alanly.ca>
2014-03-20 13:16:48 +0000

    Add necessary JS code for subcommenting.

--- soen390/public/js/player_comments.js
+++ soen390/public/js/player_comments.js
@@ -16,10 +16,10 @@ function prepareComments(path) {
 function loadComments() {
     // Fetch comments from the API.
     $.getJSON(apiPath, function(data) {
-        comments = data.return;
+        var jsonReturn = data.return;
 
         // Hide the empty comments message if necessary.
-        if (comments.length === 0) {
+        if (jsonReturn.length === 0) {
             $(".placeholder-comment").empty().show();
             $("<p/>").html("This narrative does not have any comments yet.").appendTo(".placeholder-comment");
             return;
@@ -29,48 +29,123 @@ function loadComments() {
 
         var listItems = new Array();
 
-        $.each(comments, function(index, c) {
-            listItems.push(
-                "<li class=\"media\">" +
-                "<h4>" + c.name + " <small>" + c.created_at + "</small></h4>" +
-                "<div class=\"media-body\">" +
-                c.body +
-                "</div></li>"
-            );
+        $.each(jsonReturn, function(index, c) {
+            comments[parseInt(c.comment_id)] = c;
+            listItems.push(generateCommentMediaObject(c));
         });
 
         var elements = $("<ul/>", { html: listItems.join("") }).addClass("media-list");
 
         $(".comment-frame").html(elements);
+
+        $("a").tooltip();
+
+        registerCommentHandlers();
     });
 }
 
+function generateCommentMediaObject(comment) {
+    var html     = "",
+        children = comment.children;
+
+    html += "<div class=\"media\" data-comment-id=\"" + comment.comment_id + "\">";
+    html += "<div class=\"media-body\">";
+    html += "<p class=\"media-heading\">"
+    html += comment.name + "<small>" + comment.created_at + "</small>";
+    html += "<a href=\"#\" class=\"flag-link\" title=\"Flag spam or abusive comment.\"><i class=\"fa fa-fw fa-flag\"></i></a>";
+    html += "</p>";
+    html += comment.body;
+
+    // Media footer
+
+    html += "<div class=\"media-footer\">";
+
+    if (comment.parent_id === null) {
+        html += "<a href=\"#\" class=\"comment-reply-link\">Reply</a>&mdash;";
+    }
+
+    html += "<a href=\"#\"><i class=\"fa fa-fw fa-thumbs-up\"></i> " + comment.agrees + "</a>";
+    html += "<a href=\"#\"><i class=\"fa fa-fw fa-thumbs-down\"></i> " + comment.disagrees + "</a>";
+    html += "</div>";
+
+    // Child comments
+
+    $.each(children, function(index, c) {
+        html += generateCommentMediaObject(c);
+    });
+
+    html += "</div>";
+    html += "</div>";
+
+    return html;
+}
+
 function registerCommentHandlers() {
-    // Bind the form submit event.
+
+    // Unbind handlers first
+    $(".comment-form").unbind();
+    $(".comment-reply-link").unbind();
+    $("#subcomment-form").unbind();
+
     $(".comment-form").bind("submit", function(e) {
         e.preventDefault();
 
         $("<i/>").addClass("fa fa-spin fa-spinner").appendTo(".comment-post-result");
 
-        var secondsSinceLastPost = ((new Date()).valueOf() - lastPost) / 1000;
+        postComment($(".comment-form").serialize());
+    });
 
-        if (secondsSinceLastPost < 60) {
-            var secondsUntilNextPost = parseInt(60 - secondsSinceLastPost);
-            $(".comment-post-result").addClass("text-warning").html("You may post another comment in " + secondsUntilNextPost + " seconds.");
-            return;
-        }
+    $(".comment-reply-link").bind("click", function(e) {
+        e.preventDefault();
 
-        $.post(
-            apiPath,
-            $(".comment-form").serialize()
-        ).done(function(data, status, xhr) {
-            $(".comment-post-result").addClass("text-success").html("Comment posted!");
-            lastPost = new Date();
-            $(".comment-form")[0].reset();
-            loadComments();
-        }).fail(function(xhr, status, error) {
-            $(".comment-post-result").addClass("text-danger").html("An error occured while trying to post the comment.");  
-            console.log(xhr.responseText);
-        });
+        var id      = $(this).parent().parent().parent().data("comment-id"),
+            comment = comments[parseInt(id)];
+
+        console.log("parent id = " + id);
+
+        $(".parent-comment-body").html(comment.body);
+        $(".parent-comment-name cite").attr("title", comment.created_at);
+        $(".parent-comment-name cite").html(comment.name);
+        $("#subcomment-form input[name=parent]").val(comment.comment_id);
+
+        $("#subcomment-modal").modal("show");
+    });
+
+    $("#subcomment-form").bind("submit", function(e) {
+        e.preventDefault();
+
+        console.log("subcomment-form submit");
+
+        postComment($("#subcomment-form").serialize());
+    });
+
+}
+
+function postComment(serializedData)
+{
+    var secondsSinceLastPost = ((new Date()).valueOf() - lastPost) / 1000;
+
+    if (secondsSinceLastPost < 60) {
+        $(".comment-post-result").empty();
+        var secondsUntilNextPost = parseInt(60 - secondsSinceLastPost);
+        $.bootstrapGrowl("Please wait " + secondsUntilNextPost + " seconds before your next post.", {type: "warning"});
+        return;
+    }
+
+    $.post(
+        apiPath,
+        serializedData
+    ).done(function(data, status, xhr) {
+        $.bootstrapGrowl("Comment posted!", {type: "success"});
+        lastPost = new Date();
+        $(".comment-form")[0].reset();
+        $("#subcomment-form")[0].reset();
+        $("#subcomment-modal").modal("hide");
+        loadComments();
+    }).fail(function(xhr, status, error) {
+        $.bootstrapGrowl("An error occured while posting comment. Please try again later.", {type: "danger"});
+        console.log(xhr.responseText);
+    }).always(function() {
+        $(".comment-post-result").empty();
     });
 }
