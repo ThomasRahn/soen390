40c24ebf5a78ba7944de26be6755324f67a86098
Alan Ly <me@alanly.ca>
2014-03-27 14:31:04 +0000

    Add TopicTranslation query scope and fix incorrect  attribute.

--- soen390/app/models/TopicTranslation.php
+++ soen390/app/models/TopicTranslation.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * Provides for the translation of Topic descriptions for locales supported by the system.
  *
@@ -10,7 +12,7 @@ class TopicTranslation extends \Eloquent
 {
 
     protected $guarded = array('id');
-    protected $table   = 'topic_translation';
+    protected $table   = 'topic_translations';
 
     /**
      * @codeCoverageIgnore
@@ -26,6 +28,22 @@ class TopicTranslation extends \Eloquent
     public function topic()
     {
         $this->belongsTo('Topic', 'topic_id', 'TopicID');
+    }
+
+    /**
+     * Returns an apppropriate query scope for the translation of a Topic
+     * with the given locale.
+     *
+     * @param  Builder  $query
+     * @param  string   $localeCode
+     * @return Builder
+     */
+    public function scopeInLocale(Builder $query, $localeCode)
+    {
+        return $query->where(
+            'language_id',
+            Language::where('code', $localeCode)->first()->LanguageID
+        );
     }
 
 }
